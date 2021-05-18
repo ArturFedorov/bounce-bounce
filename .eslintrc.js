568dcae3f1c12c6339fad87363199b537933b079
arturfedorov <extern.artur.fedorov@vwfs.io>
2021-05-19 02:10:15 +0400

    es lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,30 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
     'plugin:vue/vue3-recommended',
+    'prettier',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
   ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
   rules: {
-
+    'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'none' }]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/']
+      },
+      typescript: {
+        alwaysTryTypes: true
+      }
+    }
   }
 }
